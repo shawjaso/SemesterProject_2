de0e0efddd3c6aabc6b343b2d580cd7c66925085
Jason Shaw <shawjaso@uw.edu>
2012-02-04 00:21:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SemesterProject::Application.config.secret_token = '74e9c6ea3bc2bb13f7732189acc319e810a598f012b3356ac0b5e135bb8a79edf6176370b51389f43ca1fbd0433dbbc059872e666c25e355a0bec0b37f89327d'
